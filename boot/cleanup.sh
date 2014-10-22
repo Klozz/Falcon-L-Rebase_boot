46e6e7a0fbd5ca2368b2b488e545c5b278828005
h2o64 <theh2o64@gmail.com>
2014-10-22 18:15:58 +0200

    Initial commit Falcon-L-Rebase repository, limited on boot stuff to avoid downloading the 900 mo stuff --" @Klozz

---
+++ boot/cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# AIK-Linux/cleanup: reset working directory
+# osm0sis @ xda-developers
+
+cd "$PWD";
+rm -rf ramdisk split_img *new.*;
+rm *.img
+echo "Working directory cleaned.";
+return 0;
+
